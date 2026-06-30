cb7f7c4bf7367a8fb41be68c00fc70d7e82a0bf1
gkebede05 <gkebede2005@gmail.com>
2026-06-29 22:24:18 -0400

    made parse models and initialized classes

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Parse from 'parse';
+
+Parse.initialize(
+  "7RRwYI9Lvy7MtzdyT1wGE2TjUHNCJpQa8D7qW0yT",
+  "AqanjPV1KwvqJVrCSn7EW6aCxu2mn1Q1GUeUr2jK"
+);
+
+Parse.serverURL = "https://parseapi.back4app.com";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
---
+++ src/models/Message.js
@@ -0,0 +1,12 @@
+import Parse from 'parse';
+
+export const fetchAllMessages = () => {
+  const query = new Parse.Query('Message');
+  query.ascending('createdAt');
+  return query.find().then(results => results.map(msg => ({
+    id: msg.id,
+    name: msg.get('name'),
+    email: msg.get('email'),
+    message: msg.get('message'),
+  })));
+}
